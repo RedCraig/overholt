09cf89ef0397681711deba9e7f99c8f2026bd335
RedCraig <craig.mcinnes@gmail.com>
2016-04-27 21:41:36 +0100

    Added `@factory.post_generation` hook for roles generation when a user is created

--- tests/factories.py
+++ tests/factories.py
@@ -5,16 +5,16 @@
 
     Overholt test factories module
 """
+import factory
 
 from datetime import datetime
-from factory import alchemy, LazyAttribute, Sequence, SubFactory
 from flask_security.utils import encrypt_password
 from . import orm_session
 
 from overholt.models import *
 
 
-class BaseFactory(alchemy.SQLAlchemyModelFactory):
+class BaseFactory(factory.alchemy.SQLAlchemyModelFactory):
     class Meta:
         abstract = True
         sqlalchemy_session = orm_session.Session
@@ -32,23 +32,32 @@ class UserFactory(BaseFactory):
     class Meta:
         model = User
 
-    email = Sequence(lambda n: 'user{0}@overholt.com'.format(n))
-    password = LazyAttribute(lambda a: encrypt_password('password'))
+    email = factory.Sequence(lambda n: 'user{0}@overholt.com'.format(n))
+    password = factory.LazyAttribute(lambda a: encrypt_password('password'))
     last_login_at = datetime.utcnow()
     current_login_at = datetime.utcnow()
     last_login_ip = '127.0.0.1'
     current_login_ip = '127.0.0.1'
     login_count = 1
-    # roles = SubFactory(RoleFactory)
-    # roles = LazyAttribute(lambda _: [RoleFactory()])
     active = True
+
+    @factory.post_generation
+    def roles(self, create, extracted, **kwargs):
+        if not create:
+            # Simple build, do nothing.
+            return
+
+        if extracted:
+            # A list of groups were passed in, use them
+            for role in extracted:
+                self.groups.add(role)
 
 
 class StoreFactory(BaseFactory):
     class Meta:
         model = Store
 
-    name = Sequence(lambda n: 'Store Number {0}'.format(n))
+    name = factory.Sequence(lambda n: 'Store Number {0}'.format(n))
     address = '123 Overholt Alley'
     city = 'Overholt'
     state = 'New York'
@@ -59,12 +68,11 @@ class ProductFactory(BaseFactory):
     class Meta:
         model = Product
 
-    name = Sequence(lambda n: 'Product Number {0}'.format(n))
+    name = factory.Sequence(lambda n: 'Product Number {0}'.format(n))
 
 
-class CategoryFactory(alchemy.SQLAlchemyModelFactory):
+class CategoryFactory(BaseFactory):
     class Meta:
         model = Category
-        sqlalchemy_session = orm_session.Session
 
-    name = Sequence(lambda n: 'Category {0}'.format(n))
+    name = factory.Sequence(lambda n: 'Category {0}'.format(n))
