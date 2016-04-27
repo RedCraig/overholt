ef4a5000ccf68683009dace485bc0935ef60ffed
RedCraig <craig.mcinnes@gmail.com>
2016-04-27 20:42:11 +0100

    updating to python 3 `print()`

--- overholt/manage/users.py
+++ overholt/manage/users.py
@@ -26,12 +26,12 @@ class CreateUserCommand(Command):
         form = RegisterForm(data, csrf_enabled=False)
         if form.validate():
             user = register_user(email=email, password=password)
-            print '\nUser created successfully'
-            print 'User(id=%s email=%s)' % (user.id, user.email)
+            print('\nUser created successfully')
+            print('User(id=%s email=%s)' % (user.id, user.email))
             return
-        print '\nError creating user:'
+        print('\nError creating user:')
         for errors in form.errors.values():
-            print '\n'.join(errors)
+            print('\n'.join(errors))
 
 
 class DeleteUserCommand(Command):
@@ -41,10 +41,10 @@ class DeleteUserCommand(Command):
         email = prompt('Email')
         user = users.first(email=email)
         if not user:
-            print 'Invalid user'
+            print('Invalid user')
             return
         users.delete(user)
-        print 'User deleted successfully'
+        print('User deleted successfully')
 
 
 class ListUsersCommand(Command):
@@ -52,4 +52,4 @@ class ListUsersCommand(Command):
 
     def run(self):
         for u in users.all():
-            print 'User(id=%s email=%s)' % (u.id, u.email)
+            print('User(id=%s email=%s)' % (u.id, u.email))
